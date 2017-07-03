94082476f1a2aa81e5726611dd0b3fd8d9c1f9c0
DESKTOP-JNB0DI5\User <federico.dellaquila1993@gmail.com>
2017-07-03 19:49:51 +0200

    ok

---
+++ Assets/Script/SkillInfoScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+	public class SkillInfoScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	public GameObject SkillDetails;
+
+	void Update()
+	{
+		Debug.Log("SKILL_DETAILS");
+		if (ispressed == true)
+		{
+			SkillDetails.SetActive(true);
+		}
+		else
+		{
+			SkillDetails.SetActive(false);
+		}
+	}
+
+	bool ispressed = false;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		ispressed = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		ispressed = false;
+	}
+}
